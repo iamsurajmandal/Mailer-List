862895b7eaeb7d38c9e663dbb916019f8fb036e6
suraj.mandal <suraj.mandal@diatoz.com>
2021-05-07 14:49:06 +0530

    Register module

--- frontend/ui/src/Pages/RegisterPage/Register.jsx
+++ frontend/ui/src/Pages/RegisterPage/Register.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Card , FormControl, Form, InputLabel, Input, FormHelperText} from '@material-ui/core';
+import React,{useState} from 'react'
+import { Card, FormControl, Button, InputLabel, Input, FormHelperText } from '@material-ui/core';
 import Styles from "./Register.module.scss"
 function Register() {
+    const [user, setUser] = useState('')
+    const saveData = (event) => {
+        event.preventDefault();
+        console.log('e',user);
+        setUser('');
+    }
+   function emailID(event)  {
+        setUser(event.target.value)
+    }
     return (
         <Card className={Styles.root}>
-         <FormControl>
-  <InputLabel htmlFor="my-input">Email address</InputLabel>
-  <Input id="my-input" aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-</FormControl>
-      </Card>
+             <form onSubmit={saveData}>
+                <FormControl  type="text"  required="true" color="secondary" size="medium" className={Styles.emailAdd}>
+                    <InputLabel htmlFor="my-input" >Email address</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text"  value={user} onChange={emailID}/>
+                    <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                </FormControl>
+                <Button type="submit" variant="contained" color="secondary" className={Styles.btnSubmit}>Register</Button>
+            </form>
+        </Card>
     )
 }
 export default Register
