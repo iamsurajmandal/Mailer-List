35cfa54dd5737e8c44e836562fca0eb8aa983197
suraj.mandal <suraj.mandal@diatoz.com>
2021-05-07 12:33:02 +0530

    homepage and registration

--- frontend/ui/src/Pages/HomePage/HomePage.jsx
+++ frontend/ui/src/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {useHistory} from 'react-router-dom'
 import { Button } from '@material-ui/core';
-import "../styles/HomePage.scss"
+import Styles from "./HomePage.module.scss"
 function HomePage() {
     const history = useHistory();
     const goToPage=()=>{
        history.push('/register')
     }
     return (
-        <div className="body-color">
-            <h1 className="h1-tag">Welcome To Mailer List Application</h1>
-            <Button variant="contained" color="secondary" className="button-primary" onClick={goToPage}>
+        <div className={Styles.bodyHP}>
+            <h1 className={Styles.h1Line}>Welcome To Mailer List Application</h1>
+            <Button variant="contained" color="secondary" className={Styles.buttonPrimary} onClick={goToPage}>
                 Register</Button>
         </div>
     )
--- frontend/ui/src/Pages/RegisterPage/Register.jsx
+++ frontend/ui/src/Pages/RegisterPage/Register.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Card } from '@material-ui/core';
-import "../styles/Register.scss"
+import { Card , FormControl, Form, InputLabel, Input, FormHelperText} from '@material-ui/core';
+import Styles from "./Register.module.scss"
 function Register() {
     return (
-        <div>
-            <h1>Register page</h1>
-        </div>
+        <Card className={Styles.root}>
+         <FormControl>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+</FormControl>
+      </Card>
     )
 }
-
 export default Register
