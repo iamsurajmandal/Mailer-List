e52ae63809a6b8e2a636e269b0440b113589a1a2
suraj.mandal <suraj.mandal@diatoz.com>
2021-05-07 17:16:15 +0530

    sending mail

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -2,13 +2,13 @@ module.exports = (app) =>{
    app.post('/send-mail', (req,res)=>{
        (async()=>{
            try{
-               console.log('sending to mail', req.body,req.query)
            const validation = require('../validations/validators');
            const sendMail = require('../contolllers/sendmail')
-           const  sendingMail = sendMail.sendingMail();
+        //    const  sendingMail = sendMail.sendingMail();
+        res.send('sended mail')
            }
            catch(err) {
-               console.log('error while sending mail', err)
+            //    console.log('error while sending mail', err)
                res.status(500).json({
                    message:"failed to send a mail",
                    err:err
